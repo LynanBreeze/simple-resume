23fa90667972114d2417802bda7c590162dceca4
Lynan <24271362+LynanBreeze@users.noreply.github.com>
2024-02-28 22:43:19 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  server: {
-    host: "dev.lynan.cn",
-  },
   plugins: [svelte()],
 });
