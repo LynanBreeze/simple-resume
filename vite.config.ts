e6ec3463a4d738c6a8ec1e9cd2a4c2a4dfe9db0b
Lynan <24271362+LynanBreeze@users.noreply.github.com>
2024-02-28 22:45:19 +0800

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    outDir: "docs",
+  },
   plugins: [svelte()],
 });
